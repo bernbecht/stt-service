c199f0d707fd3a861894418e904610fd0683a332
Bernardo Bechtold <1577809+bernbecht@users.noreply.github.com>
2025-10-21 17:55:54 -0300

    ✨ Add STT routes to the API Gateway

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import sttRoutes from './routes/stt.routes';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/stt', sttRoutes);
 
 app.get('/', (req, res) => {
   res.send('API Gateway is running!');
