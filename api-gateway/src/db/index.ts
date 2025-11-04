53c57ee97ffadc14984ac66e2a8ce7d5bd455a74
Bernardo Bechtold <1577809+bernbecht@users.noreply.github.com>
2025-11-04 14:41:24 -0300

    💾 change location of db file

--- api-gateway/src/db/index.ts
+++ api-gateway/src/db/index.ts
@@ -2,7 +2,7 @@ import Database from 'better-sqlite3';
 import fs from 'fs';
 import path from 'path';
 
-const DB_PATH = path.resolve(__dirname, '../../../shared/database.sqlite');
+const DB_PATH = path.resolve(__dirname, '../../data/database.sqlite');
 
 // Ensure DB folder exists
 fs.mkdirSync(path.dirname(DB_PATH), { recursive: true });
